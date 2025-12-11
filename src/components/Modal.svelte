0088bd19d86267f6110ea0f9625a5053d55d2ed9
Zuramm <opheder@gmail.com>
2025-12-11 18:08:15 +0100

    add modal animation

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -23,10 +23,66 @@
 
 <dialog
 	bind:this={dialog}
-	class="ripped m-auto w-full max-w-lg space-y-4 overflow-auto bg-transparent px-4 py-6 transition-all backdrop:bg-gray-900/50 backdrop:backdrop-blur-md backdrop:backdrop-saturate-50"
+	class="ripped m-auto w-full max-w-lg space-y-4 overflow-auto bg-transparent px-4 py-6 transition-all backdrop:bg-gray-900/50"
 >
 	{#if title}
 		<h1 class="text-xl text-green-500 dark:text-green-400">{title}</h1>
 	{/if}
 	{@render children()}
 </dialog>
+
+<style>
+	/* Open state of the dialog  */
+	dialog:open {
+		opacity: 1;
+		transform: translate(0, 0) scale(1);
+	}
+
+	/* Closed state of the dialog   */
+	dialog {
+		--animation-duration: 0.2s;
+		opacity: 0;
+		transform: translate(0, 50%) scale(0.5);
+		transition:
+			opacity var(--animation-duration) var(--ease-out),
+			transform var(--animation-duration) var(--ease-out),
+			overlay var(--animation-duration) var(--ease-out) allow-discrete,
+			display var(--animation-duration) var(--ease-out) allow-discrete;
+	}
+
+	/* Before open state  */
+	/* Needs to be after the previous dialog:open rule to take effect,
+    as the specificity is the same */
+	@starting-style {
+		dialog:open {
+			opacity: 0;
+			transform: translate(0, 50%) scale(0.5);
+		}
+	}
+
+	/* Transition the :backdrop when the dialog modal is promoted to the top layer */
+	dialog::backdrop {
+		background-color: transparent;
+		backdrop-filter: blur(0) saturate(100%);
+		transition:
+			display var(--animation-duration) allow-discrete,
+			overlay var(--animation-duration) allow-discrete,
+			backdrop-filter var(--animation-duration),
+			background-color var(--animation-duration);
+	}
+
+	dialog:open::backdrop {
+		background-color: rgb(0 0 0 / 25%);
+		backdrop-filter: blur(12px) saturate(50%);
+	}
+
+	/* This starting-style rule cannot be nested inside the above selector
+because the nesting selector cannot represent pseudo-elements. */
+
+	@starting-style {
+		dialog:open::backdrop {
+			background-color: transparent;
+			backdrop-filter: blur(0) saturate(100%);
+		}
+	}
+</style>
